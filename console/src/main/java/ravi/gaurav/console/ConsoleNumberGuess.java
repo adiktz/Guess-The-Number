c8940cbe34716c2b11534ceadc2751e3351f168d
gaurav <gauravraviji@gmail.com>
2020-06-16 01:43:36 +0530

    Lombok Implementation

--- console/src/main/java/ravi/gaurav/console/ConsoleNumberGuess.java
+++ console/src/main/java/ravi/gaurav/console/ConsoleNumberGuess.java
@@ -1,7 +1,6 @@
 package ravi.gaurav.console;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
@@ -11,10 +10,9 @@ import ravi.gaurav.MessageGenerator;
 
 import java.util.Scanner;
 
+@Slf4j
 @Component
 public class ConsoleNumberGuess {
-
-    private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
     private final Game game;
     private final MessageGenerator messageGenerator;
