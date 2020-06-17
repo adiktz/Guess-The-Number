e6a6dee567e6239bf306fd70dd2ef67b3af4a6d7
gaurav <gauravraviji@gmail.com>
2020-06-17 22:59:23 +0530

    Request Interceptor

--- web/src/main/java/ravi/gaurav/web/config/WebConfig.java
+++ web/src/main/java/ravi/gaurav/web/config/WebConfig.java
@@ -1,8 +1,10 @@
 package ravi.gaurav.web.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import ravi.gaurav.web.interceptor.RequestInterceptor;
 import ravi.gaurav.web.util.ViewNames;
 
 @Configuration
@@ -11,5 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName(ViewNames.HOME);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInterceptor());
     }
 }
